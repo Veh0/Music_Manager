cbc590870ad5b304bd8b316ae1834843cf6df2ae
veh0 <valentinohleyer@live.fr>
2019-10-24 18:10:38 +0200

    Add Medium Table

---
+++ src/Entity/Media/Medium.php
@@ -0,0 +1,101 @@
+<?php
+
+
+namespace App\Entity\Media;
+
+
+use App\Entity\Album\Album;
+use App\Entity\Album\AlbumInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AbstractMediumRepository")
+ */
+abstract class AbstractMedium implements MediumInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    protected $type;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Album\Album", mappedBy="media")
+     */
+    protected $albums;
+
+    public function __construct()
+    {
+        $this->albums = new ArrayCollection();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     */
+    public function setType(string $type): void
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return Collection|Album[]
+     */
+    public function getAlbums(): Collection
+    {
+        return $this->albums;
+    }
+
+    public function addAlbum(Album $album): self
+    {
+        if (!$this->albums->contains($album)) {
+            $this->albums[] = $album;
+            $album->addMedium($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAlbum(Album $album): self
+    {
+        if ($this->albums->contains($album)) {
+            $this->albums->removeElement($album);
+            $album->removeMedium($this);
+        }
+
+        return $this;
+    }
+
+}
